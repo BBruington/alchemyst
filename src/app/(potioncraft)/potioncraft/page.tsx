67b4fbdc611dc70562f0264c7ca7942a7cf96e54
BIBruington <psychological_chemist@hotmail.com>
2025-06-08 16:26:11 -0400

    feat: add new components and styles for PotionCraft page, loading animation, and spinner

---
+++ src/app/(potioncraft)/potioncraft/page.tsx
@@ -0,0 +1,9 @@
+
+export default async function PotionCraftPage() {
+
+  return (
+    <>
+      <div>potions page</div>
+    </>
+  );
+}
---
+++ src/components/spinner.tsx
@@ -0,0 +1,14 @@
+import { Loader2 } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+type SpinnerProps = {
+  className?: string;
+};
+
+export default function Spinner({ className }: SpinnerProps) {
+  return (
+    <div className="flex items-center justify-center text-slate-500">
+      <Loader2 className={cn("h-8 w-8 animate-spin", className)} />
+    </div>
+  );
+}
